b4118be5387b87af2b6299d26fafefc839919360
Connie Leung <cleung334@gmail.com>
2024-02-20 00:38:41 +0800

    chore: add prettier to eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', {
+      "singleQuote": true,
+      "trailingComma": "all",
+      "semi": "true",
+      "printWidth": 120,
+      "useTab": false 
+    }]
   },
 };
