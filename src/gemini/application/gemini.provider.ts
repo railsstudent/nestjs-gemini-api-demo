ecb94bdb46cba6511d364187794262a833d5ba29
Connie Leung <cleung334@gmail.com>
2024-02-22 01:12:19 +0800

    chore(deps): install markdown-it

--- src/gemini/application/gemini.provider.ts
+++ src/gemini/application/gemini.provider.ts
@@ -1,8 +1,8 @@
 import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
 import { Provider } from '@nestjs/common';
 import { env } from '~configs/env.config';
-import { GEMINI_PRO_MODEL, GEMINI_PRO_VISION_MODEL } from './gemini.constant';
 import { GENERATION_CONFIG, SAFETY_SETTINGS } from '~configs/gemini.config';
+import { GEMINI_PRO_MODEL, GEMINI_PRO_VISION_MODEL } from './gemini.constant';
 
 export const GeminiProModelProvider: Provider<GenerativeModel> = {
   provide: GEMINI_PRO_MODEL,
