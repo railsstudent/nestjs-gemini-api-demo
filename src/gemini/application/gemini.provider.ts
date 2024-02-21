97eead0d19661e4e1761ac550e40a53d53c22e09
Connie Leung <cleung334@gmail.com>
2024-02-22 00:51:51 +0800

    refactor: inject gemini models in the service

--- src/gemini/application/gemini.provider.ts
+++ src/gemini/application/gemini.provider.ts
@@ -1,9 +1,29 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
 import { Provider } from '@nestjs/common';
 import { env } from '~configs/env.config';
-import { GEMINI_AI } from './gemini.constant';
+import { GEMINI_PRO_MODEL, GEMINI_PRO_VISION_MODEL } from './gemini.constant';
+import { GENERATION_CONFIG, SAFETY_SETTINGS } from '~configs/gemini.config';
 
-export const GeminiProvider: Provider = {
-  provide: GEMINI_AI,
-  useValue: new GoogleGenerativeAI(env.GEMINI.KEY),
+export const GeminiProModelProvider: Provider<GenerativeModel> = {
+  provide: GEMINI_PRO_MODEL,
+  useFactory: () => {
+    const genAI = new GoogleGenerativeAI(env.GEMINI.KEY);
+    return genAI.getGenerativeModel({
+      model: env.GEMINI.PRO_MODEL,
+      generationConfig: GENERATION_CONFIG,
+      safetySettings: SAFETY_SETTINGS,
+    });
+  },
+};
+
+export const GeminiProVisionModelProvider: Provider<GenerativeModel> = {
+  provide: GEMINI_PRO_VISION_MODEL,
+  useFactory: () => {
+    const genAI = new GoogleGenerativeAI(env.GEMINI.KEY);
+    return genAI.getGenerativeModel({
+      model: env.GEMINI.PRO_VISION_MODEL,
+      generationConfig: GENERATION_CONFIG,
+      safetySettings: SAFETY_SETTINGS,
+    });
+  },
 };
--- src/gemini/application/gemini.service.ts
+++ src/gemini/application/gemini.service.ts
@@ -1,30 +1,17 @@
-import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
+import { GenerativeModel } from '@google/generative-ai';
 import { Inject, Injectable, Logger } from '@nestjs/common';
-import { env } from '~configs/env.config';
-import { GENERATION_CONFIG, SAFETY_SETTINGS } from '~configs/gemini.config';
 import { GenAiResponse } from '~gemini/domain/interface/response.interface';
-import { GEMINI_AI } from './gemini.constant';
 import { createContent } from './helpers/content.helper';
+import { GEMINI_PRO_MODEL, GEMINI_PRO_VISION_MODEL } from './gemini.constant';
 
 @Injectable()
 export class GeminiService {
   private readonly logger = new Logger(GeminiService.name);
-  private readonly proModel: GenerativeModel;
-  private readonly proVisionModel: GenerativeModel;
 
-  constructor(@Inject(GEMINI_AI) genAI: GoogleGenerativeAI) {
-    this.proModel = genAI.getGenerativeModel({
-      model: env.GEMINI.PRO_MODEL,
-      generationConfig: GENERATION_CONFIG,
-      safetySettings: SAFETY_SETTINGS,
-    });
-
-    this.proVisionModel = genAI.getGenerativeModel({
-      model: env.GEMINI.PRO_VISION_MODEL,
-      generationConfig: GENERATION_CONFIG,
-      safetySettings: SAFETY_SETTINGS,
-    });
-  }
+  constructor(
+    @Inject(GEMINI_PRO_MODEL) private readonly proModel: GenerativeModel,
+    @Inject(GEMINI_PRO_VISION_MODEL) private readonly proVisionModel: GenerativeModel,
+  ) {}
 
   async generateText(prompt: string): Promise<GenAiResponse> {
     const contents = createContent(prompt);
@@ -56,7 +43,7 @@ export class GeminiService {
 
   async tellTheDifferences(firstImage: Express.Multer.File, secondImage: Express.Multer.File): Promise<GenAiResponse> {
     const contents = createContent(
-      'Are the two images the same? If yes, then reply "Yes". If not, then list their differences in point form.',
+      'Are the two images the same? If yes, then reply "Yes". If not, then list some of the differences in point form.',
       firstImage,
       secondImage,
     );
